47600f9781cc866120145f5c02943cb285759e16
dravidsajinraj-iex <dravid@rapidinnovation.dev>
2023-03-21 16:13:35 +0530

    webpack updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,31 @@
 var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    // to create a bundle
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  devServer: {
+    // for live reloading
+    static: "./dist",
+  },
+  devtool: "inline-source-map", // to show the error of exact location
+  plugins: [
+    // for managing the html
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    // output configurations
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true, // will generate new build every time
+    publicPath: "/", // for server
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
